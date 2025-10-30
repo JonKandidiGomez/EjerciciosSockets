46ee95515979cf24641bb462a77cc32ab1dc1060
JonKandidiGomez <jonkandidi.gomez@ikasle.egibide.org>
2025-10-30 19:57:29 +0100

    Ej2

---
+++ src/Ej2/Cliente.java
@@ -0,0 +1,26 @@
+package Ej2;
+
+import java.io.*;
+import java.net.Socket;
+
+public class Cliente {
+    public static void main(String[] args) throws IOException {
+        Socket conexion = new Socket("localhost", 5999);
+        InputStream e = conexion.getInputStream();
+        DataInputStream entrada = new DataInputStream(e);
+        OutputStream s = conexion.getOutputStream();
+        DataOutputStream salida = new DataOutputStream(s);
+        String peticion = "";
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        while (!peticion.equals("fin")) {
+            System.out.println("Escribe un texto para mandarlo al servidor o escribe 'fin': ");
+            peticion = br.readLine().toLowerCase();
+            salida.writeUTF(peticion);
+            System.out.println("Respuesta del servidor: " + entrada.readUTF());
+        }
+        entrada.close();
+        salida.close();
+        conexion.close();
+    }
+}
--- src/Ej2/Servidor.java
+++ src/Ej2/Servidor.java
@@ -1,24 +1,32 @@
+package Ej2;
+
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Servidor {
     public static void main(String[] args) throws IOException {
-        ServerSocket servidor = new ServerSocket(5500);
+        ServerSocket servidor = new ServerSocket(5999);
         Socket cliente = servidor.accept();
         InputStream e = cliente.getInputStream();
         DataInputStream entrada = new DataInputStream(e);
-        String peticion = entrada.readUTF();
-
-        System.out.println("Lo recibido desde el cliente: " + peticion);
-
         OutputStream s = cliente.getOutputStream();
         DataOutputStream salida = new DataOutputStream(s);
-        salida.writeUTF("Petición recibida.");
+        String peticion = "";
+
+        while (!peticion.equalsIgnoreCase("fin")) {
+            String respuesta;
+            peticion = entrada.readUTF().toLowerCase();
+            if(peticion.equals("fin")) {
+                salida.writeUTF("Programa finalizado.");
+            } else {
+                respuesta = peticion.toUpperCase();
+                salida.writeUTF(respuesta);
+            }
+        }
 
         entrada.close();
         salida.close();
         cliente.close();
-        servidor.close();
     }
 }
